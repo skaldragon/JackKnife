14c81164af03efd28c0e7e93b0a60d3db1c7f24a
skaldragon <37670954+skaldragon@users.noreply.github.com>
2018-03-29 11:53:11 -1000

    JackKnife Main Script to run on Host Machine

---
+++ JackKnifeMain.ps1
@@ -0,0 +1,11 @@
+$title = "JackKnife Main Menu Tool Selector"
+Write-Host $title
+Write-Host "0:JackKnife Registry Comparitor"
+Write-Host "1:JackKnife ADS Searcher"
+Write-Host "2:JackKnife WINRM Configurer"
+$result = Read-Host "Make a Selection"
+switch ($result){
+	0{ Import-Module .\JackKnifeREG.psm1}
+	1{ Import-Module .\JackKnifeADS.psm1}
+	2{ Import-Module .\JackKnifeWINRM.psm1}
+	}
