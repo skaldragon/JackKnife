dc7bfd964388f38920d543554f88e0dcccdba1d7
skaldragon <37670954+skaldragon@users.noreply.github.com>
2018-04-02 17:23:38 -1000

    Update JackKnifeMain.ps1
    Added the transfer of .zip file to enable remote access to modules

--- JackKnifeMain.ps1
+++ JackKnifeMain.ps1
@@ -1,14 +1,18 @@
 $title = "JackKnife Main Menu Tool Selector"
-mkdir "C:\Users\$env:Username\Desktop\JackKnife"
-Copy-Item -Path "C:\Users\$env:Username\Desktop\JackKnife\Modules" -ToSession $ConnectSession -Path "C:\Users\$env:Username\Desktop\JackKnife"
+$Zip="C:\Users\$env:Username\Desktop\JackKnife.zip"
+$DEST="C:\Users\$env:Username\Desktop\JackKnife"
+$Check=Test-Path -Path !$Zip
+if ($Check -ne "True"){
+Add-Type -assembly "system.io.compression.filesystem"
+[io.compression.zipfile]::ExtractToDirectory($Zip, $DEST)
+}
+cd "C:\Users\$env:Username\Desktop\JackKnife"
 Write-Host $title
 Write-Host "0:JackKnife Registry Comparitor"
 Write-Host "1:JackKnife ADS Searcher"
-Write-Host "2:JackKnife WINRM Configurer"
 $result = Read-Host "Make a Selection"
 switch ($result){
-	0{ Import-Module .\Modules\JackKnifeREG.psm1}
-	1{ Import-Module .\Modules\JackKnifeADS.psm1}
-	2{ Import-Module .\Modules\JackKnifeWINRM.psm1}
+	0{ Import-Module .\JackKnife\Modules\JackKnifeREG.psm1}
+	1{ Import-Module .\JackKnife\Modules\JackKnifeADS.psm1}
 	}
-rmdir "C:\Users\$env:Username\Desktop\JackKnife"
+
