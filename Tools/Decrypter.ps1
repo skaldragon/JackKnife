9b7c69cdd1babade55955069dae36b58712fae70
skaldragon <37670954+skaldragon@users.noreply.github.com>
2018-04-27 17:14:54 -1000

    New tools updates
    Tools used to help with day to day problems such as encryption/decryption, sending and deleting files. More to come!

---
+++ Tools/Decrypter.ps1
@@ -0,0 +1,17 @@
+﻿Function Decrypt-File{
+param(
+[Parameter(Mandatory=$true)][string]$SourcePath,
+[Parameter(Mandatory=$true)][string]$SrcFileName,
+[Parameter(Mandatory=$true)][string]$DestinationPath,
+[Parameter(Mandatory=$true)][string]$DestFilename
+)
+$String= Get-content "C:\Users\$env:Username\Desktop\JackKnife\JackKnife\Key.txt";
+$String=$String.Split(",").Replace("`(","").Replace("`)","");
+[byte[]]$key=$String;
+$script = Get-Content $SourcePath\$SrcFileName;
+$secure2= ConvertTo-SecureString $script -key $key;
+$export2=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secure2);
+$newstring= [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($export2);
+Set-Content $DestinationPath\$DestFilename $newstring;
+rm $SourcePath\$SrcFileName;
+}
---
+++ Tools/README.txt
@@ -0,0 +1,4 @@
+***To import both Encrypter.ps1, Decrypter.ps1,and Send-File.ps1 make your way within your powershell console to this directory and input these commands:
+Import-Module .\Encrypter.ps1
+Import-Module .\Decrypter.ps1
+Import-Module .\Send-File.ps1
