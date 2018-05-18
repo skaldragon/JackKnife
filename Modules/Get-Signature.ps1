9d85843057c2f9d9f29f96b95087992eff6704e8
skaldragon <37670954+skaldragon@users.noreply.github.com>
2018-05-17 17:03:15 -1000

    Gets File Signatures for selected types

---
+++ Modules/Get-Signature.ps1
@@ -0,0 +1,15 @@
+﻿Function Get-Signature{
+
+param(
+[Parameter(Mandatory=$true)][string]$Path,
+[Parameter(Mandatory=$true)][string]$Extension,
+[Parameter(Mandatory=$false,ParameterSetName="Validation")][switch]$Validate,
+[Parameter(Mandatory=$false,ParameterSetName="Validation")][string]$StatusName
+)
+ if($Validate){
+  Get-ChildItem -Path $Path -Recurse -File | Select FullName | Where-Object {$_.FullName -match "$Extension$"} | ForEach-Object {Get-AuthenticodeSignature -FilePath $_.FullName} | Where-Object {$_.Status -match "$StatusName"}
+  }
+  else{
+  Get-ChildItem -Path $Path -Recurse -File | Select FullName | Where-Object {$_.FullName -match "$Extension$"} | ForEach-Object {Get-AuthenticodeSignature -FilePath $_.FullName} | Where-Object {$_.Status -ne "Valid"}
+  }
+  }
