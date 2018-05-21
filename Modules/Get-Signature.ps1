b17ea43001942185f3446adbece72192f23768aa
skaldragon <37670954+skaldragon@users.noreply.github.com>
2018-05-21 07:41:07 -1000

    Update Get-Signature.ps1
    No need for a validation switch

--- Modules/Get-Signature.ps1
+++ Modules/Get-Signature.ps1
@@ -3,13 +3,7 @@
 param(
 [Parameter(Mandatory=$true)][string]$Path,
 [Parameter(Mandatory=$true)][string]$Extension,
-[Parameter(Mandatory=$false,ParameterSetName="Validation")][switch]$Validate,
-[Parameter(Mandatory=$false,ParameterSetName="Validation")][string]$StatusName
+[Parameter(Mandatory=$true)][string]$StatusName
 )
- if($Validate){
   Get-ChildItem -Path $Path -Recurse -File | Select FullName | Where-Object {$_.FullName -match "$Extension$"} | ForEach-Object {Get-AuthenticodeSignature -FilePath $_.FullName} | Where-Object {$_.Status -match "$StatusName"}
-  }
-  else{
-  Get-ChildItem -Path $Path -Recurse -File | Select FullName | Where-Object {$_.FullName -match "$Extension$"} | ForEach-Object {Get-AuthenticodeSignature -FilePath $_.FullName} | Where-Object {$_.Status -ne "Valid"}
-  }
   }
