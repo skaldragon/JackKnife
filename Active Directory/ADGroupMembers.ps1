1e6d45760a3f48cc2e57e60a9b6c00c60d8718ae
skaldragon <37670954+skaldragon@users.noreply.github.com>
2018-05-29 09:07:03 -1000

    Rename Active Directory/ADGroupMembers.ps1 to Old Files/ADGroupMembers.ps1

--- Active Directory/ADGroupMembers.ps1
+++
@@ -1,4 +0,0 @@
-﻿$computers=(Get-ADComputer -Filter * | Select Name).Name
-foreach($name in $computers){
-if(Test-Connection $name -Count 2 -Quiet){
-Get-WmiObject win32_group -Filter "LocalAccount='True'" -ComputerName $name | Select PSComputername,Name, @{Name="Members";Expression={ $_.GetRelated("win32_useraccount").Name-join ";"}} >>'C:\Users\$env:Username\Desktop\ADGroupMembers.txt'}}
