03fb62aad66f037ef2ad77cc659454dfda100176
skaldragon <37670954+skaldragon@users.noreply.github.com>
2018-07-12 09:45:15 -1000

    Update ADUserInfo.ps1

--- Active Directory/ADUserInfo.ps1
+++ Active Directory/ADUserInfo.ps1
@@ -1,2 +1,2 @@
-﻿$computer=(Get-ADUser -Filter(SamAccountName -ne "NULL") -Properties *)
+﻿$computer=(Get-ADUser -Filter{SamAccountName -ne "$NULL"} -Properties *)
 $computer|Export-Csv C:\Users\$env:USERNAME\Desktop\ADUserinfo.csv
