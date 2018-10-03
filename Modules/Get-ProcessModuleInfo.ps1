b2cbbe396a3507f5f4479b1aa8216a13d3406ed5
skaldragon <37670954+skaldragon@users.noreply.github.com>
2018-10-03 10:29:52 -1000

    Add files via upload

---
+++ Modules/Get-ProcessModuleInfo.ps1
@@ -0,0 +1,21 @@
+﻿function Get-ProcessModuleInfo
+{
+param(
+[parameter(Mandatory =$true)][string]$Processname,
+[parameter(Mandatory =$true)][string]$Filepath
+)
+
+    if(Get-Process -Name $Processname){
+    $Modules=(get-process -Name $Processname -Module | select modulename).modulename
+    }
+        else{
+        Write-Host "Process doesn't exist try again" -ForegroundColor Red
+        }
+foreach($item in $Modules){
+$exe=Get-WmiObject -Namespace root\cimv2 -Class cim_processexecutable | ?{$_.antecedent -match "$item"}
+        foreach($item2 in $exe){
+        [wmi]"$($item2.Antecedent)" | select FileName,Extension,Manufacturer,Version |Sort-Object filename -Unique | Export-Csv -Path $Filepath -Append
+
+                               }
+                            }
+}
