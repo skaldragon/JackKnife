54c4d71400fcc758f6ae7fa9fc607f44e36d5c0e
skaldragon <37670954+skaldragon@users.noreply.github.com>
2018-10-03 15:03:37 -1000

    Updated to prevent copies and overflow

--- Modules/Get-ProcessModuleInfo.ps1
+++ Modules/Get-ProcessModuleInfo.ps1
@@ -1,21 +1,43 @@
-﻿function Get-ProcessModuleInfo
+function Get-ProcessModuleInfo
 {
 param(
 [parameter(Mandatory =$true)][string]$Processname,
-[parameter(Mandatory =$true)][string]$Filepath
-)
+[parameter(Mandatory =$true)][string]$Filepath,
+[parameter(Mandatory =$false)][switch]$nonproprietary
 
+)
+if(!$nonproprietary){
     if(Get-Process -Name $Processname){
-    $Modules=(get-process -Name $Processname -Module | select modulename).modulename
+    $Modules=(get-process -Name $Processname -Module | select modulename | sort modulename -Unique).modulename
     }
         else{
         Write-Host "Process doesn't exist try again" -ForegroundColor Red
         }
 foreach($item in $Modules){
-$exe=Get-WmiObject -Namespace root\cimv2 -Class cim_processexecutable | ?{$_.antecedent -match "$item"}
+$exe=Get-WmiObject -Namespace root\cimv2 -Class cim_processexecutable | ?{$_.antecedent -match "$item"} | Sort-Object antecedent -Unique
         foreach($item2 in $exe){
-        [wmi]"$($item2.Antecedent)" | select FileName,Extension,Manufacturer,Version |Sort-Object filename -Unique | Export-Csv -Path $Filepath -Append
-
+        [wmi]"$($item2.Antecedent)" | select FileName,Extension,Manufacturer,Version | Export-Csv -Path $Filepath -Append
                                }
                             }
-}
+
+                    }
+
+
+if($nonproprietary){
+if(Get-Process -Name $Processname){
+    $Modules=(get-process -Name $Processname -Module | select modulename | sort modulename -Unique).modulename
+    }
+        else{
+        Write-Host "Process doesn't exist try again" -ForegroundColor Red
+        }
+foreach($item in $Modules){
+$exe=Get-WmiObject -Namespace root\cimv2 -Class cim_processexecutable | ?{$_.antecedent -match "$item"} | Sort-Object antecedent -Unique
+        foreach($item2 in $exe){
+        [wmi]"$($item2.Antecedent)" | ? {$_.manufacturer -ne "Microsoft Corporation"}| select FileName,Extension,Manufacturer,Version | Export-Csv -Path $Filepath -Append
+        
+
+                               }
+                           }
+
+                    }
+ }#End Get-ProcessModuleInfo
