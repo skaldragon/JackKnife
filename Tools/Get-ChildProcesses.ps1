6fab4c6e95220ccfb020ca009692de70535e6940
skaldragon <37670954+skaldragon@users.noreply.github.com>
2019-04-29 10:36:53 -1000

    Update Get-ChildProcesses.ps1

--- Tools/Get-ChildProcesses.ps1
+++ Tools/Get-ChildProcesses.ps1
@@ -1,8 +1,8 @@
-﻿function Get-ChildProcesses{
+function Get-ChildProcesses{
 param(
 [Parameter()]$RunningProcesses=(Get-WmiObject win32_process | select processid).processid
 )
 foreach($ID in $RunningProcesses){
-Get-WmiObject win32_process | Where-Object{$_.processid -eq "$ID"| Select ProcessName,@{Name="ChildProcess";Expression=[((Get-WmiObject win32_process|Where-Object{$_.parentprocessid -eq "$ID"} | select processname).Processname) -join "`n"}} | Export-Csv -Path C:\users\$env:username\desktop\ParentToChildProcesses.csv -append
+Get-WmiObject win32_process | Where-Object{$_.processid -eq "$ID"}| Select ProcessName,@{Name="ChildProcess";Expression={((Get-WmiObject win32_process|Where-Object{$_.parentprocessid -eq "$ID"} | select processname).Processname) -join "`n"}}
 }
 }
