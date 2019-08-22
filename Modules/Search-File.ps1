c89df78aeef21f89b4b158fabc71997c70b1725d
skaldragon <37670954+skaldragon@users.noreply.github.com>
2019-08-22 07:55:35 -1000

    Added synopsis of script

--- Modules/Search-File.ps1
+++ Modules/Search-File.ps1
@@ -1,6 +1,39 @@
 #The goal of this script is to be able to look for any file withing a computer system no matter the name or pattern of name it may contain.
 
 function Search-File{
+<#
+	.SYNOPSIS
+		This function has the capability to find any type of file on any file system
+	.PARAMETER HostPath
+		The local or UNC folder path that you'd like to search files in recursively.
+	.PARAMETER Extension
+		The type of file you are looking for, foreach loops with multiple extensions will work for this as well.
+	.PARAMETER Include and Exclude
+		Whether or not to include the extension or exclude it from search results.
+	.PARAMETER Filename
+		Used if you're searching with SearchforNormal switch to find normal files with specific filenames and extensions.
+	.SWITCH SearchForRandom
+		Uses Regex to find files with varying file names, can be used with different extensions and file lengths.
+	.SWITCH SearchForNormal
+		Searches for specific file names and extensions.
+	.PARAMETER HostComputers
+		Array to add multiple computers one by one separated by comma if you don't want to search the whole AD.
+    .SWITCH All
+		Used to search all of AD.
+    .SWITCH Hostlist
+		Used with HostComputers to search list of computers instead of AD.
+    .SWITCH LocalHost
+		Used to search only the local system.
+	.EXAMPLE
+	 Search-File -Hostpath C:\users -extension .exe -SearchforRandom -localhost
+	.EXAMPLE 
+	 Search-File -Hostpath C:\users -Filename evil.exe -SearchforNormal -All
+	.EXAMPLE 
+	 Search-File -Hostpath C:\users -extension .ppt -exclude -SearchforRandom -HostList -Hostcomputers server1,server2,server3
+	.OUTPUTS
+	Outputs results in the PWD in a .txt file.
+		#>
+
 [cmdletBinding(DefaultParameterSetName="Set One")]
 param(
 [Parameter(Mandatory=$true,ParameterSetName="Set One")][Parameter(ParameterSetName="Set Two")][string]$HostPath,
