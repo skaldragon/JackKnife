49560797acafd9afe3c79e268978bc8e3f24c84c
skaldragon <37670954+skaldragon@users.noreply.github.com>
2020-10-31 12:54:33 -0400

    Information Added

--- Scripts/ADUserInfo.ps1
+++ Scripts/ADUserInfo.ps1
@@ -1,2 +1,3 @@
-﻿$computer=(Get-ADUser -Filter{SamAccountName -ne "$NULL"} -Properties *)
+﻿#Gathers all AD User information
+ $computer=(Get-ADUser -Filter{SamAccountName -ne "$NULL"} -Properties *)
 $computer|Export-Csv C:\Users\$env:USERNAME\Desktop\ADUserinfo.csv
