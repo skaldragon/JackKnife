a4e599bd4825e813d9d5a0e68398274cbb0109fb
skaldragon <37670954+skaldragon@users.noreply.github.com>
2020-10-31 10:28:03 -0400

    Delete ADGroupMembers.ps1

--- Active Directory/ADGroupMembers.ps1
+++
@@ -1,1 +0,0 @@
-﻿Get-ADGroup -Filter {Members -ne "NULL"} -properties Name | Select-Object Name, @{Name="GroupMembers";Expression={(Get-ADGroupMember -identity "$_" | Select-Object -ExpandProperty SamAccountName) -join "`n"}} | Export-Csv -Path C:\users\$env:username\desktop\ADGroupMembers.csv -Append
