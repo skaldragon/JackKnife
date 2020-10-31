e617eb2ddd259d52e45083189e2d479761e58a95
skaldragon <37670954+skaldragon@users.noreply.github.com>
2020-10-31 10:28:10 -0400

    Delete ADLoggedonUsers.ps1

--- Active Directory/ADLoggedonUsers.ps1
+++
@@ -1,7 +0,0 @@
-﻿$Computer=(Get-ADComputer -Filter * | Select Name).Name
-Foreach($item in $Computer){
-if ( Test-Connection -Count 2 -ComputerName $item){
-$name=(Get-WmiObject -ComputerName $item win32_loggedonuser | Select antecedent) | Out-string;
-$name.Replace("\\.\root\cimv2:Win32_Account.Domain=","") >> C:\Users\$env:Username\Desktop\LoggedonUsers.txt
-                                                   }
-}
