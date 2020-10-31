3797d06d46312542625d1ecd89417ef8f45483dc
skaldragon <37670954+skaldragon@users.noreply.github.com>
2020-10-31 10:34:19 -0400

    Gathers Kernel Driver information

---
+++ Scripts/Get-Drivers.ps1
@@ -0,0 +1,26 @@
+﻿function Get-Drivers{
+if($ErrorActionPreference -ne 'silentlycontinue'){
+$ErrorActionPreference='silentlycontinue'
+}
+$var=(gwmi win32_baseservice | where{$_.servicetype -eq "kernel driver"} | select pathname).pathname
+[system.collections.arraylist]$Drivers=@()
+foreach($item in $var){
+$Object=Get-Item -Path $item 
+$signature=Get-AuthenticodeSignature -FilePath $item 
+$signature2=Get-AuthenticodeSignature -FilePath $item
+$signature=($signature | select -ExpandProperty Signercertificate).subject
+$signature=$signature -replace '..=',''
+$Table=[PSCustomObject]@{
+Driver=$Object.versioninfo.InternalName
+Version=$Object.versioninfo.ProductVersion
+Product=$Object.versioninfo.productname
+Status=$signature2.Status
+StatusMessage=$signature2.StatusMessage
+Signedby=$signature
+}
+$Drivers.Add($Table) | Out-Null
+
+}
+$Drivers
+$ErrorActionPreference='Continue'
+}
