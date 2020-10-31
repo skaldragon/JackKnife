0e96d42b1365ebb661335e3910888a2b125dea77
skaldragon <37670954+skaldragon@users.noreply.github.com>
2020-10-31 12:53:26 -0400

    Added Information

--- Scripts/ADGroupMembers.ps1
+++ Scripts/ADGroupMembers.ps1
@@ -1,1 +1,2 @@
-﻿Get-ADGroup -Filter {Members -ne "NULL"} -properties Name | Select-Object Name, @{Name="GroupMembers";Expression={(Get-ADGroupMember -identity "$_" | Select-Object -ExpandProperty SamAccountName) -join "`n"}} | Export-Csv -Path C:\users\$env:username\desktop\ADGroupMembers.csv -Append
+#Gathers Active Directory Group Information﻿
+Get-ADGroup -Filter {Members -ne "NULL"} -properties Name | Select-Object Name, @{Name="GroupMembers";Expression={(Get-ADGroupMember -identity "$_" | Select-Object -ExpandProperty SamAccountName) -join "`n"}} | Export-Csv -Path C:\users\$env:username\desktop\ADGroupMembers.csv -Append
