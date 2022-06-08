e646fede60c30f51ca0c71274eb311cfe75fe8ef
skaldragon <37670954+skaldragon@users.noreply.github.com>
2022-06-08 11:19:21 -0400

    Create Get-BetterNetstat.ps1

---
+++ Scripts/Get-BetterNetstat.ps1
@@ -0,0 +1,29 @@
+function Get-BetterNetstat{
+<#
+.SYNOPSIS
+Does what Netstat -anob does except without having to be Administrator.
+    #>
+$TCP=Get-NetTCPConnection | select *
+$Processes=gwmi win32_process | select Processname,ProcessID
+
+[System.Collections.ArrayList]$TCPARRAY = @()
+
+foreach($item in $TCP){
+if($item.OwningProcess -iin $Processes.ProcessID)
+{
+$Name=($Processes |?{$_.ProcessID -eq $item.OwningProcess} | select ProcessName).Processname
+
+
+$TCPFields=[PSCustomObject]@{
+LocalAddress=$item.LocalAddress
+LocalPort=$item.LocalPort
+RemoteAddress=$item.RemoteAddress
+RemotePort=$item.RemotePort
+ProcessName=$Name
+State=$item.state | ?{$_.state -ne "Bound"}
+}
+}
+$TCPARRAY.Add(($TCPFields)) | Out-Null
+}
+$TCPARRAY | Format-Table -AutoSize
+}
